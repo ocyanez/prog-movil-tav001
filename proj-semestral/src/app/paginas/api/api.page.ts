2433aa1d4492e8ad397e9bc5de884afb9189a8c8
GabrielGrobier <163485684+GabrielGrobier@users.noreply.github.com>
2025-01-09 13:24:59 +0000

    se agrega persistencia a la api consumida desde el api.service.ts , y se modifica la api.page.ts para que funcione la persistencia como tal

--- proj-semestral/src/app/paginas/api/api.page.ts
+++ proj-semestral/src/app/paginas/api/api.page.ts
@@ -1,5 +1,7 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/servicios/api.service';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-api',
@@ -9,18 +11,33 @@ import { ApiService } from 'src/app/servicios/api.service';
 })
 export class ApiPage implements OnInit {
 
-  constructor(private api:ApiService) { }
+  constructor(private api:ApiService , private storage:Storage) { 
+    this.init_storage();
+  }
 
   ngOnInit() {
     this.traer_datos();
+    this.obtener_codigo();
+
   }
   datos:any[]=[];
 
-  traer_datos(){
-    this.api.solicitud().subscribe((data)=>{
-      console.log(data);
-      this.datos=data.meals;
+  async traer_datos(){
+    this.datos =  await this.storage.get("respuestaApi");}
+    
+
+  init_storage(){
+    this.storage.create();
+  }
+  async obtener_codigo(){
+    this.api.solicitud().subscribe(async (Response:HttpResponse<any>)=>{
+      console.log("estado",Response.status)
+      if(Response.status==200){
+        console.log("esta bien la peticion ")
+        await this.storage.set("respuestaApi",Response.body.meals)
+      }
     })
+
   }
 
 }
