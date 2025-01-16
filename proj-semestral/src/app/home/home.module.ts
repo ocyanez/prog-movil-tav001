b892cf5a5ca11f09154e655279a87f7d5b1ae2ed
GabrielGrobier <163485684+GabrielGrobier@users.noreply.github.com>
2025-01-16 14:21:13 +0000

    se agrega mapbox como componente

--- proj-semestral/src/app/home/home.module.ts
+++ proj-semestral/src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { MapaComponent } from '../componenetes/mapa/mapa.component';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MapaComponent
   ],
   declarations: [HomePage]
 })
